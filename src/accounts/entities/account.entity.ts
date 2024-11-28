a5c37a98bbd44ce56166276e55f78a6c80286955
Rafael Menegon <dev.rafaelmenegon@gmail.com>
2024-11-28 01:58:04 -0300

    fix: accounts entity timestamp to datetime

--- src/accounts/entities/account.entity.ts
+++ src/accounts/entities/account.entity.ts
@@ -1,22 +1,22 @@
-import { Entity, Column, PrimaryGeneratedColumn, VersionColumn } from 'typeorm';
+import { Entity, Column, PrimaryGeneratedColumn, VersionColumn } from "typeorm";
 
-@Entity('accounts')
+@Entity("accounts")
 export class Account {
-  @PrimaryGeneratedColumn('uuid')
+  @PrimaryGeneratedColumn("uuid")
   id: string;
 
   @Column({ unique: true })
   accountNumber: string;
 
-  @Column('decimal', { precision: 10, scale: 2, default: 0 })
+  @Column("decimal", { precision: 10, scale: 2, default: 0 })
   balance: number;
 
   @VersionColumn()
   version: number;
 
-  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  @Column({ type: "datetime", default: () => "CURRENT_TIMESTAMP" })
   createdAt: Date;
 
-  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  @Column({ type: "datetime", default: () => "CURRENT_TIMESTAMP" })
   updatedAt: Date;
 }
