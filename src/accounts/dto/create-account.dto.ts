6f296a3cf420042842b59bb4b22fef6741cd1284
Rafael Menegon <dev.rafaelmenegon@gmail.com>
2024-11-28 20:35:45 -0300

    feat: add users relation to accounts

--- src/accounts/dto/create-account.dto.ts
+++ src/accounts/dto/create-account.dto.ts
@@ -1,7 +1,27 @@
-import { IsString, IsNotEmpty } from 'class-validator';
+import {
+  IsString,
+  IsNotEmpty,
+  IsBoolean,
+  IsOptional,
+  IsArray,
+  IsUUID,
+} from "class-validator";
 
 export class CreateAccountDto {
   @IsString()
   @IsNotEmpty()
   accountNumber: string;
+
+  @IsUUID()
+  @IsNotEmpty()
+  userId: string;
+
+  @IsArray()
+  @IsUUID("4", { each: true })
+  @IsOptional()
+  additionalUserIds?: string[];
+
+  @IsBoolean()
+  @IsOptional()
+  isSharedAccount?: boolean;
 }
