4c121efa7c7dc1177be4ab5fb10155b456bf9892
Rafael Menegon <dev.rafaelmenegon@gmail.com>
2024-11-28 23:10:37 -0300

    feat: configure swagger in main

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from "@nestjs/core";
 import { ValidationPipe } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
 import { AppModule } from "./app.module";
 import { Config } from "./common/interfaces/config.interface";
 
@@ -19,11 +20,22 @@ async function bootstrap() {
   const apiPrefix = configService.get("app.apiPrefix", { infer: true });
   app.setGlobalPrefix(apiPrefix);
 
+  const config = new DocumentBuilder()
+    .setTitle("Banking System API")
+    .setDescription("API documentation for the Banking System")
+    .setVersion("1.0")
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("docs", app, document);
+
   const port = configService.get("app.port", { infer: true });
   await app.listen(port);
 
   console.log(
     `Application is running on: http://localhost:${port}/${apiPrefix}`
   );
+  console.log(`Swagger documentation: http://localhost:${port}/docs`);
 }
 bootstrap();
