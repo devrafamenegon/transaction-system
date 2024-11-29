13587ecce85284811d2f4496b91e88f326b05b5a
Rafael Menegon <dev.rafaelmenegon@gmail.com>
2024-11-29 01:42:43 -0300

    feat: add user entity into accounts module import

--- src/accounts/accounts.module.ts
+++ src/accounts/accounts.module.ts
@@ -5,9 +5,10 @@ import { AccountsService } from "./accounts.service";
 import { AccountRepository } from "./repositories/account.repository";
 import { Account } from "./entities/account.entity";
 import { AuthModule } from "../auth/auth.module";
+import { User } from "../auth/entities/user.entity";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Account]), AuthModule],
+  imports: [TypeOrmModule.forFeature([Account, User]), AuthModule],
   controllers: [AccountsController],
   providers: [AccountsService, AccountRepository],
   exports: [AccountsService, AccountRepository],
