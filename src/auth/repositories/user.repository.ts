cd40c245ff04cc7196f9b8bf5bba5a82c110ed51
Rafael Menegon <dev.rafaelmenegon@gmail.com>
2024-11-29 03:14:13 -0300

    feat: add some get methods and loggers into user repository

--- src/auth/repositories/user.repository.ts
+++ src/auth/repositories/user.repository.ts
@@ -2,33 +2,59 @@ import { Injectable } from "@nestjs/common";
 import { Repository } from "typeorm";
 import { InjectRepository } from "@nestjs/typeorm";
 import { User } from "../entities/user.entity";
+import { LoggerService } from "../../common/logger/logger.service";
 
 @Injectable()
 export class UserRepository {
   constructor(
     @InjectRepository(User)
-    private readonly repository: Repository<User>
+    private readonly repository: Repository<User>,
+    private readonly logger: LoggerService
   ) {}
 
   async findById(id: string): Promise<User | null> {
-    return this.repository.findOne({
-      where: { id },
-      relations: ["accounts"],
-    });
+    this.logger.debug(`Finding user by ID: ${id}`, "UserRepository");
+    return this.repository
+      .createQueryBuilder("user")
+      .leftJoinAndSelect("user.accounts", "accounts")
+      .where("user.id = :id", { id })
+      .getOne();
   }
 
   async findByEmail(email: string): Promise<User | null> {
-    return this.repository.findOne({
-      where: { email },
-    });
+    this.logger.debug(`Finding user by email: ${email}`, "UserRepository");
+    return this.repository
+      .createQueryBuilder("user")
+      .where("user.email = :email", { email })
+      .getOne();
   }
 
   async create(userData: Partial<User>): Promise<User> {
+    this.logger.debug(
+      `Creating new user: ${JSON.stringify(userData)}`,
+      "UserRepository"
+    );
     const user = this.repository.create(userData);
     return this.repository.save(user);
   }
 
   async findByIds(ids: string[]): Promise<User[]> {
-    return this.repository.findByIds(ids);
+    this.logger.debug(
+      `Finding users by IDs: ${ids.join(", ")}`,
+      "UserRepository"
+    );
+    return this.repository
+      .createQueryBuilder("user")
+      .leftJoinAndSelect("user.accounts", "accounts")
+      .whereInIds(ids)
+      .getMany();
+  }
+
+  async findAll(): Promise<User[]> {
+    this.logger.debug("Finding all users", "UserRepository");
+    return this.repository
+      .createQueryBuilder("user")
+      .leftJoinAndSelect("user.accounts", "accounts")
+      .getMany();
   }
 }
